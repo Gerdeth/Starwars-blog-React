26edb6bc9801d95e6e4ccbd480d6af018c9ef2c5
Gerdeth <gerdethwickham@gmail.com>
2020-08-07 14:10:52 +0000

    pics etc wth updates

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -19,27 +19,27 @@ export class Home extends React.Component {
 	}
 	render() {
 		var charCards = null;
-		if (this.state.char.length > 1) {
-			charCards = this.state.char.map((character, cardIndex) => {
-				return (
-					<div key={cardIndex}>
-						<Card
-							imageSrc={`https://loremflickr.com/200/200/starwars?random=${cardIndex +
-								1}`}
-							character={character}
-						/>
-					</div>
-				);
-			});
-		} else {
-			console.log("loading");
-		}
-
 		return (
 			<div className="container-fluid">
 				<Navbar />
 
-				<div className="cardsdisplay">{charCards}</div>
+				<div className="cardsdisplay">
+					{this.state.char.length > 1 ? (
+						this.state.char.map((character, cardIndex) => {
+							return (
+								<div key={cardIndex}>
+									<Card
+										imageSrc={`https://loremflickr.com/200/200/starwars?random=${cardIndex +
+											1}`}
+										character={character}
+									/>
+								</div>
+							);
+						})
+					) : (
+						<h1>Loading...</h1>
+					)}
+				</div>
 				<hr />
 				<PlanetHome />
 			</div>
--- src/js/views/planetHome.js
+++ src/js/views/planetHome.js
@@ -17,22 +17,27 @@ export class PlanetHome extends React.Component {
 	}
 	render() {
 		var plntCards = null;
-		if (this.state.plnt.length > 1) {
-			plntCards = this.state.plnt.map((hPlanet, plntcardIndex) => {
-				return (
-					<div key={plntcardIndex}>
-						<PlanetCard
-							imageSrc={`https://loremflickr.com/200/200/starwars?random=${plntcardIndex +
-								1}`}
-							hPlanet={hPlanet}
-						/>
-					</div>
-				);
-			});
-		} else {
-			console.log("loading");
-		}
 
-		return <div className="planetdisplay">{plntCards}</div>;
+		return (
+			<div className="planetdisplay">
+				{this.state.plnt.length > 1 ? (
+					(plntCards = this.state.plnt.map(
+						(hPlanet, plntcardIndex) => {
+							return (
+								<div key={plntcardIndex}>
+									<PlanetCard
+										imageSrc={`https://loremflickr.com/200/200/planets?random=${plntcardIndex +
+											1}`}
+										hPlanet={hPlanet}
+									/>
+								</div>
+							);
+						}
+					))
+				) : (
+					<h1>Loading....</h1>
+				)}
+			</div>
+		);
 	}
 }
